35d0a8af427a5aeb859a6a72c84d4cb682b80af0
Taya <justataya@gmail.com>
2020-05-06 06:20:17 -0700

    Add quote

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function onAvatarMouseOver() {
+    document.getElementById("win-quote").style.display = "inline";
+}
+
+function onAvatarMouseOut() {
+    document.getElementById("win-quote").style.display = "none";
+}
+
